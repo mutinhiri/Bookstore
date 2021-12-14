0771af71a46d163fa0fb9ec51aec66df250c24b4
mutinhiri <barnabmutinhiri@gmail.com>
2021-12-14 20:50:31 +0200

    Add reducer function

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -6,6 +6,10 @@ const initialState = [];
 const booksReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_BOOK:
+      return [...state, action.payload];
+    case REMOVE_BOOK:
       return state.filter((book) => book.id !== action.payload);
+    default:
+      return state;
   }
 }
