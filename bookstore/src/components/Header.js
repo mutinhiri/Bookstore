a2431428fa12d68d1a388ef5efea9488daa73709
mutinhiri <barnabmutinhiri@gmail.com>
2021-12-14 14:23:34 +0200

    Add header component with links

--- bookstore/src/components/Header.js
+++ bookstore/src/components/Header.js
@@ -3,7 +3,11 @@ import React from 'react'
 export default function Header() {
   return (
     <header className='header'>
-      
+      <h1>BookStore</h1>
+      <nav>
+        <a href="/">Books</a>
+        <a href="/Categories">categories</a>
+      </nav>
     </header>
   )
 }
