4b160a4912df0bf1645adfc9f88d38c6adb34b9d
mutinhiri <barnabmutinhiri@gmail.com>
2021-12-15 20:56:10 +0200

    Fix errors in linters

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-export default function Header() {
-  return (
-    <header className="header">
-      <h1>BookStore</h1>
-      <nav className="nav">
-        <a href="/">Books</a>
-        <a href="/Categories">categories</a>
-      </nav>
-    </header>
-  );
-}
+const Header = () => (
+  <header className="header">
+    <h1>BookStore</h1>
+    <nav className="nav">
+      <a href="/">Books</a>
+      <a href="/Categories">categories</a>
+    </nav>
+  </header>
+);
+
+export default Header;
